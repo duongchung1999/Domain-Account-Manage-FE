ac73e0f5b3845e0e207042fad12cf355949ac23d
Duong.Pham <2588846@foxlink-dn1.com>
2024-04-09 16:54:08 +0700

    Foxlink Computer Manage

---
+++ src/container/MenuSide.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './MenuSide.css';
+import { NavLink } from 'react-router-dom';
+
+
+class MenuSide extends Component {
+    render() {
+        return (
+            <div className='menuSide sb-sidenav'>
+                <div className='menuSide-item'>
+                    <ul className="menuSide-nav">
+                        <NavItem path="/home" itemName="User Information" icon = {<i className="nav-icon fa-solid fa-users"></i>}/>
+                        <NavItem path="#!" itemName="Computer Information" icon = {<i className="nav-icon fa-solid fa-network-wired"></i>}/>
+                        <NavItem path="#!" itemName="Printer IP Address" icon = {<i className="nav-icon fa-solid fa-print"></i>}/>
+                        <NavItem path="#!" itemName="Documents" icon = {<i className="nav-icon fa-regular fa-folder-open"></i>}/>
+                        
+                      </ul>
+                      
+                </div>
+
+                <div className='menuSide-footer'>
+                    <div className="small">Logged in as</div>
+                            Foxlink IT
+                </div>
+
+                
+            </div>
+        );
+    }
+}
+
+function NavItem(props){
+    return(
+        <li>
+            <NavLink to={props.path}  className="nav-link">{props.icon}  {props.itemName}</NavLink>
+        </li>
+    )
+}
+export default MenuSide;
