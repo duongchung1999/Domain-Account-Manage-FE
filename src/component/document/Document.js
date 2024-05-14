3140751ebbd1decf25c5236aaca05707c4cc1589
Duong.Pham <2588846@foxlink-dn1.com>
2024-05-14 15:28:56 +0700

    add-file-complete

--- src/component/document/Document.js
+++ src/component/document/Document.js
@@ -8,6 +8,9 @@ import './Document.css';
 import DocumentTable  from './DocumentTable';
 import { Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
+import  Uploader from '../../publicComponent/Uploader/Uploader'
+
+
 const apiUrl = process.env.REACT_APP_API_URL;
 class Document extends Component {
     constructor(props) {
@@ -42,148 +45,24 @@ class Document extends Component {
             isOpenUploadFileForm: !prevState.isOpenUploadFileForm
         }));
     }
-    setFileName=event =>{
+    setParentOpenForm = (isOpen) => {
         this.setState({
-            fileName: event.target.value
-        })
+            isOpenUploadFileForm: isOpen
+        });
     }
 
     uploadFileForm = ()=>{
         if(this.state.isOpenUploadFileForm){
             return(
                 <div className='col-3'>
-                    <div className='file-inputFile' 
-                    style={{ border: '2px dashed #cccccc' }}
-                    onClick={() => document.getElementById('fileInput').click()}
-                    onDrop={this.handleDrop}
-                    // onDragOver={(e) => e.preventDefault()}
-                    >
-
-                        <input className='file-inputFile-select' type="file" onChange={this.handleSelectFile} style={{ display: 'none' }} id="fileInput" />
-                        
-                    </div>
-                    <div className='fileNameInput row'>
-                        <input className='col' type='text' 
-                        name="fileName"
-                         value={this.state.fileName} 
-                         onChange={this.setFileName}
-                         placeholder={"File Name"}></input>
-                       
-                    </div>
-                    <div className='row'>
-                        <Button 
-                        className='col'
-                            variant="warning" 
-                            style={{ marginLeft: '10px' }}
-                            onClick={() => document.getElementById('fileInput').click()}
-                        >Select</Button>
-                        <Button 
-                        className='col'
-                            variant="danger" 
-                            style={{ marginLeft: '10px' }}
-                            onClick={this.handleFileUpload}
-                        >Upload</Button>
-                    </div>
+                    <Uploader setParentOpenForm={this.setParentOpenForm}/>
                     
                 </div>
             )
         }
         else return null;
     }
-    handleDrop = (event) => {
-        event.preventDefault();
-        console.log(event);
-        const file = event.dataTransfer.files[0];
-        this.handleSelectFile(file);
-    }
-    handleSelectFile = (event) =>{
-        const file = event.target.files[0];
-        const fileName = file.name;
-        const fileType = file.type;
-        const fileExtension = fileName.split('.').pop();
-        // Xử lý tệp ở đây, ví dụ:
-        this.setState({
-            file: file,
-            fileName: fileName,
-            fileType: fileType,
-            fileExtension: fileExtension,
-        })
-        console.log('Tên tệp:', fileExtension);
-        console.log('Tên tệp:', file.name);
-        console.log('Loại tệp:', file.type);
-        console.log('Kích thước tệp (bytes):', file.size);
-    }
-    handleFileUpload = (event) => {
-        const file = this.state.file;
-        const fileName = this.state.fileName;
-        Swal.fire({
-            title: `Do you want to upload ${fileName}?`,
-            showDenyButton: true,
-            showCancelButton: false,
-            confirmButtonText: "Yes",
-            denyButtonText: `No`
-          }).then(async(result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.isConfirmed) {
-                const formData = new FormData();
-                // formData.append('name',fileName);
-                formData.append('file', file);
-                const getToken = localStorage.getItem("token");
-                const token = JSON.parse(getToken)
-            
-                try{
-                    const response = await fetch(apiUrl+'/api/Document/upload', {
-                        method: 'POST',
-                        headers: {
-                            'Authorization': 'Bearer ' + token.value,
-                        },
-                        body: formData
-                    });
-                    if (response.status === 201 || response.status === 204) {
-                        Swal.fire({
-                            position: "center",
-                            icon: "success",
-                            title: response.statusText,
-                            showConfirmButton: false,
-                            timer: 1000
-                        });
-                        console.log('Phản hồi từ API:', response);
-                    } else {
-                        console.error('Lỗi:', response);
-                        Swal.fire({
-                            position: "center",
-                            icon: "error",
-                            title: response.status+ " " + response.statusText,
-                            showConfirmButton: false,
-                            timer: 2000
-                        });
-                    }
-                }
-                catch (error){
-                    console.error('Lỗi:', error.message);
-                    Swal.fire({
-                        position: "center",
-                        icon: "error",
-                        title: error.message,
-                        showConfirmButton: false,
-                        timer: 2000
-                    });
-
-                }
-               
-                
-            } else if (result.isDenied) {
-              Swal.fire({
-                position: "center",
-                icon: "error",
-                title: "Upload Failed!",
-                showConfirmButton: false,
-                timer: 1500
-              });
-            }
-          });
-        
-      };
+   
     render() {
         return (
             // <App 
---
+++ src/publicComponent/Uploader/Uploader.js
@@ -0,0 +1,138 @@
+import { useState } from 'react';
+import './Uploader.css';
+import { MdCloudUpload,MdDelete } from 'react-icons/md';
+import { AiFillFileImage } from 'react-icons/ai';
+import { Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+const apiUrl = process.env.REACT_APP_API_URL;
+
+function Uploader({ setParentOpenForm }){
+    const [image,setImage] = useState(null)
+    const [fileName,setFileName] = useState("No selected file")
+    const [file,setFile]=useState(null)
+    const [openForm,setOpenForm] = useState(true)
+    const  handleFileUpload = (event) => {
+        // const file = file;
+        // const fileName = this.state.fileName;
+        Swal.fire({
+            title: `Do you want to upload ${fileName}?`,
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Yes",
+            denyButtonText: `No`
+          }).then(async(result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                const formData = new FormData();
+                // formData.append('name',fileName);
+                formData.append('file', file);
+                const getToken = localStorage.getItem("token");
+                const token = JSON.parse(getToken)
+            
+                try{
+                    const response = await fetch(apiUrl+'/api/Document/upload', {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': 'Bearer ' + token.value,
+                        },
+                        body: formData
+                    });
+                    if (response.status === 201 || response.status === 204||response.status===200) {
+                        Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: response.statusText,
+                            showConfirmButton: false,
+                            timer: 1000
+                        });
+                        // setOpenForm(false);
+                        setParentOpenForm(false) ;
+                        console.log('Phản hồi từ API:', response);
+                    } else {
+                        console.error('Lỗi:', response);
+                        Swal.fire({
+                            position: "center",
+                            icon: "error",
+                            title: response.status+ " " + response.statusText,
+                            showConfirmButton: false,
+                            timer: 2000
+                        });
+                    }
+                }
+                catch (error){
+                    console.error('Lỗi:', error.message);
+                    Swal.fire({
+                        position: "center",
+                        icon: "error",
+                        title: error.message,
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+
+                }
+               
+                
+            } else if (result.isDenied) {
+              Swal.fire({
+                position: "center",
+                icon: "error",
+                title: "Upload Failed!",
+                showConfirmButton: false,
+                timer: 1500
+              });
+            }
+          });
+        
+      };
+    return(
+        
+        <main>
+            <form
+            className='uploader-inputFile col' 
+            onClick={()=> document.querySelector(".file-inputFile-select").click()}
+            >
+                <input
+                        type="file"
+                        className="file-inputFile-select"
+                        hidden
+                        onChange={({target: {files}})=>{
+                            files[0] && setFileName(files[0].name);
+                            if(files){
+                                setImage(URL.createObjectURL(files[0]))
+                                setFile(files[0])
+                            }
+                        }}
+                    />
+                {image?
+                <img src={image} width={60} height={60} alt={fileName}/>
+                :
+                <div className='uploader-select-area'>
+                <MdCloudUpload className='uploader-select-icon' color='#1475cf' size={90}/> 
+                <p>Browse file to upload</p>
+                </div>
+                }
+            </form>
+
+            <section className='uploader-fileName'>
+                <AiFillFileImage size={30} color='#1475cf'/>
+                <span >
+                    {fileName}
+                    <MdDelete 
+                    size={30}
+                    onClick={()=>{
+                        setFileName("No Selected file");
+                        setImage(null);
+                    }}/>
+                </span>
+
+            </section>
+            <Button 
+                        className='col'
+                            variant="danger" 
+                            onClick={handleFileUpload}
+                        >Upload</Button>
+            
+        </main>
+    )
+}
+export default Uploader;
